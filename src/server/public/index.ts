61de2661325ed4b75a87d056b216ef96f632a62c
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-10-13 16:16:25 +0100

    Modularised server lib files

---
+++ src/server/public/index.ts
@@ -0,0 +1,10 @@
+/** PUBLIC EXPLANATION
+ *
+ * Public is used to encapsulate any modules that are safe to be used in a user server context.
+ * These are public APIs that can be used in server components, server actions, and API routes.
+ *
+ **/
+
+// Export all public server modules
+
+export * from "./getSession.js";
---
+++ src/server/runtime/index.ts
@@ -0,0 +1,10 @@
+/** RUNTIME EXPLANATION 
+ * 
+ * Runtime is used to encapsulate any modules that are used at runtime by the auth library.
+ * These are internal APIs that should not be used in a user server context.
+ * 
+ **/ 
+
+// Exports for all server runtime modules
+
+export * from "./callbacks.js";
