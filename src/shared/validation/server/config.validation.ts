db9addfbd645f417dce35f1a21c3b0100a45d2de
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-11-17 23:42:57 +0000

    Changed the config schema to add TTLs to idle/absolute timeouts

--- src/shared/validation/server/config.validation.ts
+++ src/shared/validation/server/config.validation.ts
@@ -23,11 +23,22 @@ export const DatabasePoolConfigSchema = z.object({
 
 /** Full config schema */
 export const AuthConfigSchema = z.object({
-  options: z
-    .object({
-      strategy: z.enum(["jwt", "database"]),
-    })
-    .default({ strategy: "database" }),
+  options: [
+    z
+      .object({
+        strategy: z.enum(["jwt", "database"]),
+      })
+      .default({ strategy: "database" }),
+
+    // This gives the developer the option to how long they wish the sessions for datbabase persistant storage to last for
+    z
+      .object({
+        idleTTL: z.int().optional(),
+        absoluteTTL: z.int().optional(),
+      })
+      .default({ idleTTL: 900, absoluteTTL: 32400 }), // idleTTL = 15 mins, absoluteTTL = 9 hours
+  ],
+
   /** Either a database pool, or a database URL */
   db: z.union([z.string().url(), DatabasePoolConfigSchema]),
   providers: z.array(z.any()).default([]), // TODO: refine this with proper type
