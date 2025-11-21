fb99d24ba2bf54bcf70e4532c2f08c489a69de5e
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-11-21 14:34:38 +0000

    Created methods to register and log into email-password accounts

--- src/classes/providers/emailPassword.ts
+++ src/classes/providers/emailPassword.ts
@@ -1,15 +1,101 @@
+import { DatabaseAccountInteractions } from "@/db/interfaces/databaseAccountInteractions";
+import { DatabaseUserInteractions } from "@/db/interfaces/databaseUserInteractions";
+import { NewUser, User } from "@/db/schemas";
+
 /**
  * @class EmailPasswordProvider
  * @description This class provides email and password authentication functionalities.
  */
 export class EmailPasswordProvider {
-  login(email: string, password: string): boolean {
-    return true;
-    // TODO: implement logic here
+  /** Used to log a User when provided with an email and password */
+  async login(email: string, password: string): Promise<User | null> {
+    // Attempt to retrieve a User with the provided email
+    const userWithEmail = await DatabaseUserInteractions.getUserByEmail(email);
+
+    // No user exists with such email
+    if (!userWithEmail) {
+      return null;
+    }
+
+    // TODO: actual authentication logic with password hashing
+
+    // Attempt to retrieve an Account with the retrieved User ID
+    const userAccount =
+      await DatabaseAccountInteractions.getAccountByCompositeKey(
+        userWithEmail.id,
+        "emailPassword"
+      );
+
+    // No Account exists for email-password provider with the given User ID, or the given password is incorrect for the Account
+    if (!userAccount || userAccount.password !== password) {
+      return null;
+    }
+
+    return userWithEmail;
   }
 
-  register(email: string, password: string): boolean {
-    return true;
-    // TODO: implement logic here
+  // TODO: make this into a transaction ? not desperate
+  /** Used to register a new user for email-password authentication */
+  async register(
+    config: NewUser,
+    password: string,
+    passwordHash?: string
+  ): Promise<User> {
+    // Attempt to retrieve a user with the provided email before anything else
+    const userResult = await DatabaseUserInteractions.getUserByEmail(
+      config.email
+    );
+
+    // No given user, therefore no account, or perhaps error.
+    if (!userResult) {
+      // Attempt to create a new User with the given details
+      const newUserResult = await DatabaseUserInteractions.createUser(config);
+
+      // DB error whilst trying to append User to Users table
+      if (!newUserResult) {
+        throw new Error(
+          `An error occurred whilst trying to append the User with email ${config.email} to the Users table. User register failed.`
+        );
+      }
+
+      // Attempt to create an email-password Account for the new User
+      const newAccountResult = await DatabaseAccountInteractions.createAccount({
+        userId: newUserResult.id,
+        provider: "emailPassword",
+        password,
+        passwordHash,
+      });
+
+      // DB error occurred whilst trying to append Account
+      if (!newAccountResult) {
+        // Delete the User from the Users table as no account has been created
+        await DatabaseUserInteractions.deleteUser(newUserResult.id);
+
+        throw new Error(
+          `An error occurred whilst trying to append the Account with email ${config.email} to the Account table. User register failed.`
+        );
+      }
+
+      return newUserResult;
+    }
+
+    // User account exists, try to create a email-password account
+
+    // Attempt to create an email-password Account for the existing
+    const newAccountResult = await DatabaseAccountInteractions.createAccount({
+      userId: userResult.id,
+      provider: "emailPassword",
+      password,
+      passwordHash,
+    });
+
+    // DB error occurred whilst trying to append the Account
+    if (!newAccountResult) {
+      throw new Error(
+        `An error occurred whilst trying to append the Account with email ${config.email} to the Account table. Email-password register failed.`
+      );
+    }
+
+    return userResult;
   }
 }
