6713b3129aa9cd147ba12cca244af71c241e78c0
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-11-24 12:16:59 +0000

    Fixed minor error missing export and created constants for TTLs

--- src/utils/session/generateSessionCookie.ts
+++ src/utils/session/generateSessionCookie.ts
@@ -3,7 +3,7 @@ import { authConfig } from "@/core/singleton";
 /**
  * Generates cookie options for a secure, HttpOnly session cookie
  */
-function generateSessionCookie(
+export function generateSessionCookie(
   name: string,
   value: string,
   maxAgeSeconds: number,
